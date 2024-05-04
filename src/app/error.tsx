a6de53f89335f18dc72f14e1fdb87dcdaad20107
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-05-05 08:08:17 +0900

    add loading/error/not-found

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div>
+      <div className="text-center">500</div>
+      <div className="text-center">Internal Server Error</div>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div className="text-center">404</div>
+      <div className="text-center">Not Found</div>
+    </div>
+  );
+};
+
+export default NotFound;
